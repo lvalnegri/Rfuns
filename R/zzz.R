bb6c68b62c521ba3719b7a44c7b890d95e04f871
Luca Valnegri <l.valnegri@datamaps.co.uk>
2019-09-22 14:14:28 +0100

    added paths for Italian stuff

--- R/zzz.R
+++ R/zzz.R
@@ -13,15 +13,39 @@ bnduk_path <- file.path(pub_path, 'boundaries', 'uk')
 #' @export
 bnduk_spath <- file.path(bnduk_path, 'rds', 's20')
 
-#' Location of the geographic datasets in the shared repository
+#' Location of the UK datasets in the shared repository
 #'
 #' @export
 datauk_path <- file.path(pub_path, 'datasets', 'uk')
 
-#' Location of the geographic datasets in the shared repository
+#' Location of the geographic UK datasets in the shared repository
 #'
 #' @export
 geouk_path <- file.path(datauk_path, 'geography')
+
+
+
+#' Location of the Italian boundaries in the shared repository
+#'
+#' @export
+bndit_path <- file.path(pub_path, 'boundaries', 'it')
+
+#' Location of the Italian boundaries, in the shared repository, tipycally used in R applications (= rds format)
+#'
+#' @export
+bndit_spath <- file.path(bndit_path, 'rds', 's20')
+
+#' Location of the Italian datasets in the shared repository
+#'
+#' @export
+datait_path <- file.path(pub_path, 'datasets', 'it')
+
+#' Location of the geographic Italian datasets in the shared repository
+#'
+#' @export
+geoit_path <- file.path(datait_path, 'geography')
+
+
 
 #' Coordinate Reference Systems (CRS) in EPSG code for OSGB 1936 / OSGB 1936 / Airy 1830 / British National Grid
 #'
