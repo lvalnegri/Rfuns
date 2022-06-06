7db9b60c90d67bbd2a9bae75c3c280d15506226f
datamaps <l.valnegri@datamaps.co.uk>
2022-06-06 14:11:58 +0100

    changed package name

---
+++ R/Rfuns.R
@@ -0,0 +1,10 @@
+#' Rfuns: a bunch of functions to help Data Sourcing, Cleaning and Wrangling for data projects
+#'
+#' The `Rfuns` package provides the following categories of important functions:
+#'  - connection and querying to local MySQL and main SQL Server databases
+#'  - cleaning, recoding, saving, and querying R datasets
+#'  - quick summaries and mapping
+#'
+#' @docType package
+#' @name Rfuns
+NULL
