4cb4f1d941ca2ee9eda9308be6c5667787578186
datamaps <l.valnegri@datamaps.co.uk>
2022-06-29 16:21:42 +0100

    moved constants from `zzz`
    added helpers for plugins

--- R/zzz.R
+++ R/zzz.R
@@ -237,74 +237,10 @@ bbox.it <- matrix(
 )
 
 
-#' List of background tiles for leaflet maps
-#'
-#' For the complete list see https://leaflet-extras.github.io/leaflet-providers/preview/
-#'
-#' @export
-tiles.lst <- list(
-    'OSM Mapnik' = 'OpenStreetMap.Mapnik',
-    'OSM HOT' = 'OpenStreetMap.HOT',
-    'OSM Topo' = 'OpenTopoMap',
-    'Esri Street' = 'Esri.WorldStreetMap',
-    'Esri De Lorme' = 'Esri.DeLorme',
-    'Esri Topo' = 'Esri.WorldTopoMap',
-    'Esri Imagery' = 'Esri.WorldImagery',
-    'CartoDB Voyager' = 'CartoDB.Voyager',
-    'CartoDB Positron' = 'CartoDB.Positron',
-    'CartoDB Dark Matter' = 'CartoDB.DarkMatter',
-    'Stamen Terrain' = 'Stamen.Terrain',
-    'Stamen Toner Lite' = 'Stamen.TonerLite',
-    'Stamen Toner' = 'Stamen.Toner',
-    'Stamen Toner NoLabel' = 'Stamen.TonerBackground',
-    'Stamen Watercolor' = 'Stamen.Watercolor'
-)
-
 #' List of available positions on the screen for objects that stays in a corner
 #'
 #' @export
 pos.lst <- c('Bottom Right' = 'bottomright', 'Bottom Left' = 'bottomleft', 'Top Left' = 'topleft', 'Top Right' = 'topright')
-
-#' List of available fill colours for Awesome Markers in leaflet maps
-#'
-#' @export
-marker_colours <- c(
-    'red', 'darkred', 'orange', 'pink', 'beige', 'green', 'darkgreen', 'lightgreen',
-    'blue', 'lightblue', 'purple', 'cadetblue', 'white', 'lightgray', 'gray', 'black'
-)
-
-#' List of available methods when classifying numeric variables
-#'
-#' @export
-class.lst <- c(
-    'Fixed' = 'fixed',                  # need an additional argument fixedBreaks that lists the n+1 values to be used
-    'Equal Intervals' = 'equal',        # the range of the variable is divided into n part of equal space
-    'Quantiles' = 'quantile',           # each class contains (more or less) the same amount of values
-    'Pretty Integers' = 'pretty',       # sequence of about ‘n+1’ equally spaced ‘round’ values which cover the range of the values in ‘x’. The values are chosen so that they are 1, 2 or 5 times a power of 10.
-    'Natural Breaks' = 'jenks',         # seeks to reduce the variance within classes and maximize the variance between classes
-    'Hierarchical Cluster' = 'hclust',  # Cluster with short distance
-    'K-means Cluster' = 'kmeans'        # Cluster with low variance and similar size
-)
-
-#' List of ColorBrewer palettes partitioned by type of visualization scale
-#'
-#' @export
-palettes.lst <- list(
-    'SEQUENTIAL' = c( # ordinal data where (usually) low is less important and high is more important
-        'Blues' = 'Blues', 'Blue-Green' = 'BuGn', 'Blue-Purple' = 'BuPu', 'Green-Blue' = 'GnBu', 'Greens' = 'Greens', 'Greys' = 'Greys',
-        'Oranges' = 'Oranges', 'Orange-Red' = 'OrRd', 'Purple-Blue' = 'PuBu', 'Purple-Blue-Green' = 'PuBuGn', 'Purple-Red' = 'PuRd', 'Purples' = 'Purples',
-        'Red-Purple' = 'RdPu', 'Reds' = 'Reds', 'Yellow-Green' = 'YlGn', 'Yellow-Green-Blue' = 'YlGnBu', 'Yellow-Orange-Brown' = 'YlOrBr',
-        'Yellow-Orange-Red' = 'YlOrRd'
-    ),
-    'DIVERGING' = c(  # ordinal data where both low and high are important (i.e. deviation from some reference "average" point)
-        'Brown-Blue-Green' = 'BrBG', 'Pink-Blue-Green' = 'PiYG', 'Purple-Red-Green' = 'PRGn', 'Orange-Purple' = 'PuOr', 'Red-Blue' = 'RdBu', 'Red-Grey' = 'RdGy',
-        'Red-Yellow-Blue' = 'RdYlBu', 'Red-Yellow-Green' = 'RdYlGn', 'Spectral' = 'Spectral'
-    ),
-    'QUALITATIVE' = c(  # categorical/nominal data where there is no logical order
-        'Accent' = 'Accent', 'Dark2' = 'Dark2', 'Paired' = 'Paired', 'Pastel1' = 'Pastel1', 'Pastel2' = 'Pastel2',
-        'Set1' = 'Set1', 'Set2' = 'Set2', 'Set3' = 'Set3'
-    )
-)
 
 #' List of available shapes with outline colour
 #'
@@ -320,40 +256,6 @@ lnstypes.lst <- c('dashed', 'dotted', 'solid', 'dotdash', 'longdash', 'twodash')
 #'
 #' @export
 fcstypes.lst <- c('plain', 'bold', 'italic', 'bold.italic')
-
-
-# Generic options for the labels appearing when hovering polygons in leaflet maps
-#'
-#' @export
-lbl.options <- leaflet::labelOptions(
-    nohide = TRUE,
-    textsize = '12px',
-    direction = 'right',
-    sticky = FALSE,
-    opacity = 0.8,
-    offset = c(10, -10),
-    style = list(
-        'color' = 'black',
-        'border-color' = 'rgba(0,0,0,0.5)',
-        'font-family' = 'verdana',
-        'font-style' = 'normal',
-        'font-size' = '14px',
-        'font-weight' = 'normal',
-        'padding' = '2px 6px',
-        'box-shadow' = '3px 3px rgba(0,0,0,0.25)'
-    )
-)
-
-# Generic options for the border highlight when hovering polygons in leaflet maps
-#'
-#' @export
-hlt.options <- leaflet::highlightOptions(
-    weight = 6,
-    color = 'white',
-    opacity = 1,
-    bringToFront = TRUE,
-    sendToBack = TRUE
-)
 
 
 # .onLoad <- function(libname, pkgname) {
