bd36fd82e5b2c64bb4b6c6b9d8cf395e32a8f459
datamaps <l.valnegri@datamaps.co.uk>
2022-07-24 17:44:45 +0100

    additions

--- R/strings.R
+++ R/strings.R
@@ -68,3 +68,78 @@ add_pct <- function(x, dgt = 1){ paste0(formatC(100 * x, digits = dgt, format = 
 #' @export
 #'
 add_Kcomma <- function(x){ formatC(x, big.mark = ',') }
+
+
+#' str_dx
+#' 
+#' Returns the last $n$ characters of a string
+#'
+#' @param x the string
+#' @param n the desired number of characters
+#'
+#' @return a string
+#'
+#' @author Luca Valnegri, \email{l.valnegri@datamaps.co.uk}
+#'
+#' @export
+#'
+str_dx <- function(x, n) substr(x, nchar(x)-n+1, nchar(x))
+
+
+#' str_add_char
+#' 
+#' Add a number of prefixed characters to one side of a string
+#'
+#' @param x the string
+#' @param n the desired *total* number of characters to return
+#' @param chr the character(s) to add 
+#' @param pos Indicate if \code{chr} should be inserted at the (l)eft (beginning of string) or (r)right (end of string)
+#'
+#' @return a string
+#'
+#' @author Luca Valnegri, \email{l.valnegri@datamaps.co.uk}
+#'
+#' @export
+#'
+str_add_char <- function(x, n, chr = '0', pos = 'l'){
+    switch(pos,
+        'l' = str_dx(paste0(strrep(chr, n), x), n),
+        'r' = substr(paste0(x, strrep(chr, n)), 1, n),
+        stop('Position <', pos, '> not valid!')
+    )
+}
+
+
+#' str_clean
+#' 
+#' Clean a string from non alphanumeric characters
+#' 
+#' @param x the string
+#' @param subst Indicate if dots and spaces should be kept but sobstituted with a different character
+#' @param subst_car the character(s) to insert instead of dots and spaces
+#'
+#' @return a string
+#'
+#' @author Luca Valnegri, \email{l.valnegri@datamaps.co.uk}
+#'
+#' @export
+#'
+str_clean <- function(x, subst = TRUE, subst_chr = '-'){
+    if(subst) x <- gsub('[ \\.]', subst_chr, x)
+    gsub(paste0('[^[:alnum:]', ifelse(subst, subst_chr, ''), ']'), '', x)
+}
+
+
+#' nchar_int
+#' 
+#' Return the length in character of an integer (works also for very long integer)
+#'
+#' @param x an integer
+#'
+#' @return an integer
+#'
+#' @author Luca Valnegri, \email{l.valnegri@datamaps.co.uk}
+#'
+#' @export
+#'
+nchar_int <- function(x) as.integer(gsub('.*\\+(.*)', '\\1', formatC(x, format = 'e'))) + 1
